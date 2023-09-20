712d6274001c054a538d1ceae3ea1d5523e542e7
GLinBoy <hojjatabedie@gmail.com>
2023-09-20 20:27:52 +0200

    👌 IMPROVE: Added a method to get image files of a directory

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-import logging
+import logging, os
 
 
 logging.basicConfig(format='%(levelname)s | %(asctime)s | %(message)s', level=logging.INFO)
+
+
+def get_files(path):
+    logging.info('Reading File from: %s', path)
+    images = []
+    for file in os.listdir(path):
+        if (file.endswith('.png') or file.endswith('.jpg')):
+            images.append('{}/{}'.format(path, file))
+    return images
 
 
 def main():
